12c1a015755935101e2224bad97619a1c7776e87
박우림 <woorimprog@gmail.com>
2021-04-27 17:49:48 +0900

    Style(woorim/header): 탭 네비게이션 디자인 변경

--- app/src/navigations/MainTab.js
+++ app/src/navigations/MainTab.js
@@ -1,23 +1,45 @@
-import React, { useContext, useEffect } from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Profile, Main, NoticeBoard, Boards } from '../screens';
-import { MaterialIcons } from '@expo/vector-icons'
-import { ThemeContext } from 'styled-components';
-import { Alert } from 'react-native';
+import React, { useContext, useEffect } from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Profile, Main, NoticeBoard, Boards } from "../screens";
+import { Ionicons, MaterialIcons, AntDesign } from "@expo/vector-icons";
+import { ThemeContext } from "styled-components";
+import { Alert } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
-const TabBarIcon = ({ focused, name }) => {
+const TabBarIconIonicons = ({ focused, name }) => {
   const theme = useContext(ThemeContext);
   return (
-    <MaterialIcons
+    <Ionicons
       name={name}
-      size={26}
+      size={focused ? 32 : 24}
       color={focused ? theme.tabActiveColor : theme.tabInactiveColor}
     />
   );
 };
-  
+
+const TabBarIconMaterialIcons = ({ focused, name }) => {
+  const theme = useContext(ThemeContext);
+  return (
+    <MaterialIcons
+      name={name}
+      size={focused ? 32 : 24}
+      color={focused ? theme.tabActiveColor : theme.tabInactiveColor}
+    />
+  );
+};
+
+const TabBarIconAntDesign = ({ focused, name }) => {
+  const theme = useContext(ThemeContext);
+  return (
+    <AntDesign
+      name={name}
+      size={focused ? 32 : 24}
+      color={focused ? theme.tabActiveColor : theme.tabInactiveColor}
+    />
+  );
+};
+
 const MainTab = ({}) => {
   const theme = useContext(ThemeContext);
 
@@ -27,42 +49,43 @@ const MainTab = ({}) => {
         activeTintColor: theme.tabActiveColor,
         inactiveTintColor: theme.tabInactiveColor,
         headerShown: false,
+        showLabel: false,
       }}
     >
-        <Tab.Screen
-          name="Idu"
-          component={Main}
-          options={{
-            tabBarIcon: ({ focused }) =>
-              TabBarIcon({
-                focused,
-                name: 'home',
-              }),
-          }}
-        />
-        <Tab.Screen
-          name="공지"
-          component={NoticeBoard}
-          options={{
-            tabBarIcon: ({ focused }) =>
-              TabBarIcon({
-                focused,
-                name: 'campaign',
-              }),
-          }}
-        />
-        <Tab.Screen
-          name="프로필"
-          component={Profile}
-          options={{
-            tabBarIcon: ({ focused }) =>
-              TabBarIcon({
-                focused,
-                name: focused ? 'person' : 'person-outline',
-              }),
-          }}
-          onPress= {() => {}}
-        />
+      <Tab.Screen
+        name="Idu"
+        component={Main}
+        options={{
+          tabBarIcon: ({ focused }) =>
+            TabBarIconIonicons({
+              focused,
+              name: focused ? "home" : "home-outline",
+            }),
+        }}
+      />
+      <Tab.Screen
+        name="공지"
+        component={NoticeBoard}
+        options={{
+          tabBarIcon: ({ focused }) =>
+            TabBarIconAntDesign({
+              focused,
+              name: "notification",
+            }),
+        }}
+      />
+      <Tab.Screen
+        name="프로필"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ focused }) =>
+            TabBarIconIonicons({
+              focused,
+              name: focused ? "person" : "person-outline",
+            }),
+        }}
+        onPress={() => {}}
+      />
     </Tab.Navigator>
   );
 };
