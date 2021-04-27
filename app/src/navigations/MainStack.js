fff6adb19015e70ca1730a8e74483e01b2684f37
박우림 <woorimprog@gmail.com>
2021-04-27 17:50:41 +0900

    Style(woorim/header): 헤더 디자인 변경 #12

--- app/src/navigations/MainStack.js
+++ app/src/navigations/MainStack.js
@@ -1,30 +1,56 @@
-import React, { useContext } from 'react'
-import { ThemeContext } from 'styled-components/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { Main, Boards, Markets, NoticeBoard } from '../screens';
-import MainTab from './MainTab';
-
+import React, { useContext } from "react";
+import { ThemeContext, DrawerButton } from "styled-components/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Main, Boards, Markets, NoticeBoard } from "../screens";
+import { MaterialIcons } from "@expo/vector-icons";
+import MainTab from "./MainTab";
 
 const Stack = createStackNavigator();
 
 function MainStack() {
-    const theme = useContext(ThemeContext);
+  const theme = useContext(ThemeContext);
 
-    return (
-        <Stack.Navigator
-            initialRouteName="Main"
-            screenOptions={{
-                headerTitle: 'IDU',
-                headerTintColor: theme.headerTintColor,
-                cardStyle: { backgroundColor: theme.backgroundColor},
-                headerBackTitleVisible: false,
-            }}
-        >
-            <Stack.Screen name="Main" component={MainTab} />
-            <Stack.Screen name="Boards" component={Boards} />
-            <Stack.Screen name="Markets" component={Markets} />
-        </Stack.Navigator>
-    )
+  return (
+    <Stack.Navigator
+      initialRouteName="Main"
+      screenOptions={{
+        headerTitle: "IDU",
+        headerTintColor: theme.headerTintColor,
+        headerBackTitleVisible: false,
+        headerStyle: {
+          height: 80,
+          borderBottomWidth: 2,
+          borderBottomColor: theme.headerBottomColor,
+        },
+        cardStyle: { backgroundColor: theme.backgroundColor },
+        headerRight: ({ tintColor }) => (
+          <MaterialIcons
+            name="notifications-none"
+            size={30}
+            style={{ marginRight: 11 }}
+            color={theme.headerIconColor}
+            onPress={() => alert("준비중인 서비스입니다.")}
+          />
+        ),
+      }}
+    >
+      <Stack.Screen
+        name="Main"
+        component={MainTab}
+        options={{
+          headerTitle: "🐳 아이두",
+          headerTitleStyle: {
+            color: theme.headerTintColor,
+            fontSize: 24,
+            fontWeight: "bold",
+          },
+          headerTitleAlign: "left",
+        }}
+      />
+      <Stack.Screen name="Boards" component={Boards} />
+      <Stack.Screen name="Markets" component={Markets} />
+    </Stack.Navigator>
+  );
 }
 
-export default MainStack
+export default MainStack;
