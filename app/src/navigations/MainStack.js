8383f39745ee71f6e9b0206d996fc537c3916f30
박우림 <woorimprog@gmail.com>
2021-04-30 21:18:00 +0900

    Style(woorim/font): 배민 한나체 폰트 추가 #22

--- app/src/navigations/MainStack.js
+++ app/src/navigations/MainStack.js
@@ -1,28 +1,42 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { ThemeContext, DrawerButton } from "styled-components/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Main, Markets, NoticeBoard , ViewDetail, PostWrite } from "../screens";
+import { Main, Markets, NoticeBoard, ViewDetail, PostWrite } from "../screens";
 import { MaterialIcons } from "@expo/vector-icons";
+import AppLoading from "expo-app-loading";
+
 import MainTab from "./MainTab";
+// import * as Font from "../utils/font";
+// 폰트
+import * as Font from "expo-font";
 
 const Stack = createStackNavigator();
 
-function MainStack() {
+const MainStack = () => {
+  const [isFontReady, setIsFontReady] = useState(false);
   const theme = useContext(ThemeContext);
 
-  return (
+  async function _loadFonts() {
+    await Font.loadAsync({
+      BM_HANNA_PRO: require("../../assets/fonts/BMHANNAPro.ttf"),
+    });
+  }
+
+  return isFontReady ? (
     <Stack.Navigator
       initialRouteName="Main"
       screenOptions={{
         headerTitle: "IDU",
+        headerTitleStyle: {
+          fontFamily: "BM_HANNA_PRO",
+        },
         headerTintColor: theme.headerTintColor,
         headerBackTitleVisible: false,
         headerStyle: {
           height: 80,
-          borderBottomWidth: 2,
+          borderBottomWidth: 1,
           borderBottomColor: theme.headerBottomColor,
         },
-        cardStyle: { backgroundColor: theme.backgroundColor },
         headerRight: ({ tintColor }) => (
           <MaterialIcons
             name="notifications-none"
@@ -32,27 +46,35 @@ function MainStack() {
             onPress={() => alert("준비중인 서비스입니다.")}
           />
         ),
+        cardStyle: { backgroundColor: theme.backgroundColor },
       }}
     >
       <Stack.Screen
         name="Main"
         component={MainTab}
         options={{
-          headerTitle: "🐳 아이두",
+          headerTitle: "아이두",
           headerTitleStyle: {
             color: theme.headerTintColor,
             fontSize: 24,
             fontWeight: "bold",
+            fontFamily: "BM_HANNA_PRO",
           },
           headerTitleAlign: "left",
         }}
       />
-      <Stack.Screen name="ViewDetail" component={ViewDetail}/>
+      <Stack.Screen name="ViewDetail" component={ViewDetail} />
       <Stack.Screen name="PostWrite" component={PostWrite} />
       <Stack.Screen name="Markets" component={Markets} />
       <Stack.Screen name="NoticeBoard" component={NoticeBoard} />
     </Stack.Navigator>
+  ) : (
+    <AppLoading
+      startAsync={_loadFonts}
+      onFinish={() => setIsFontReady(true)}
+      onError={console.error}
+    />
   );
-}
+};
 
 export default MainStack;
