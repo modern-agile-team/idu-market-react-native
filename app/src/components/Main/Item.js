abaf9fa8d17f9cd595b49b74d5d286542a6b8fe3
박우림 <woorimprog@gmail.com>
2021-05-03 16:54:43 +0900

    Add(woorim/main): Item 컴포넌트 추가

---
+++ app/src/components/Main/Item.js
@@ -0,0 +1,68 @@
+import React from "react";
+import styled from "styled-components/native";
+import { MaterialIcons, FontAwesome5 } from "@expo/vector-icons";
+
+const Container = styled.Pressable`
+  flex: 1;
+  margin-right: 20px;
+  width: 150px;
+`;
+
+const StyledImage = styled.Image.attrs((props) => ({
+  source: props.source,
+  resizeMode: "cover",
+}))`
+  height: 100px;
+  width: 100%;
+  border-radius: 12px;
+  border-width: 1px;
+  border-color: gray;
+`;
+
+const ItemTitle = styled.Text.attrs(() => ({
+  numberOfLines: 1,
+}))`
+  font-size: 18px;
+  font-weight: bold;
+  margin: 3px 0 0 3px;
+`;
+
+const ItemContent = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  margin: 3px 0 0 3px;
+`;
+
+const ItemContentText = styled.Text`
+  font-size: 16px;
+  padding: 3px;
+  color: ${({ theme }) => theme.marketFont};
+`;
+
+const Item = ({
+  hitSlop,
+  onPress,
+  imgUrl,
+  itemTitle,
+  studentId,
+  commentCount,
+}) => {
+  return (
+    <Container hitSlop={hitSlop} onPress={onPress}>
+      <StyledImage source={{ uri: imgUrl }} />
+      <ItemTitle>{itemTitle}</ItemTitle>
+      <ItemContent>
+        <MaterialIcons name="person-outline" size={16} color="black" />
+        <ItemContentText>{studentId}</ItemContentText>
+      </ItemContent>
+      <ItemContent>
+        <FontAwesome5 name="comment-dots" size={16} color="black" />
+        <ItemContentText>{commentCount}</ItemContentText>
+      </ItemContent>
+    </Container>
+  );
+};
+
+export default Item;
