9651bfc11b6041ec6785cce0a99c11c3b28877c8
jiwonpg98 <jiwonpg98@gmail.com>
2021-05-04 17:31:09 +0900

    Refactor(jiwon/market): 교재,it기기 등 UI 화면 구성 #26

--- app/src/screens/Markets.js
+++ app/src/screens/Markets.js
@@ -1,48 +1,58 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components/native";
-import { View, ScrollView, FlatList } from "react-native";
-import { images } from "../constants";
-import { MaterialIcons } from "@expo/vector-icons";
+import React, { useContext, useState, useEffect } from "react";
+import { FlatList } from "react-native-gesture-handler";
+import styled, { ThemeContext } from "styled-components/native";
+import { AntDesign, MaterialIcons, FontAwesome5 } from "@expo/vector-icons";
 import FAB from "react-native-fab";
+import AppLoding from "expo-app-loading";
+import { ProgressContext } from "../contexts";
+import { View } from "react-native";
 
 const Container = styled.SafeAreaView`
   flex: 1;
   background-color: ${({ theme }) => theme.background};
-  padding: 20px;
-  justify-content: center;
-`;
-//category
-const CategoryContainer = styled.TouchableOpacity`
-  flex: 1;
-  justify-content: center;
-  padding-left: 35px;
-  margin-right: 55px;
 `;
 
-const CategoryText = styled.Text`
-  font-size: 22px;
-  color: ${({ theme }) => theme.marketFont};
-`;
-
-const ClickedCategoryText = styled.Text`
-  font-size: 22px;
-  color: ${({ theme }) => theme.marketClickFont};
-`;
-
-//item
 const ItemContainer = styled.TouchableOpacity`
-  flex: 1;
-  border-top-width: 1px;
+  border-bottom-width: 1px;
+  border-color: ${({ theme }) => theme.listBorder};
+  padding: 10px 10px 15px 10px;
   flex-direction: row;
-  padding: 25px 10px 3px 10px;
-  border-color: ${({ theme }) => theme.marketBorder};
 `;
-// 묶어주는 Container
+
+const ItemTextContainer = styled.View`
+  flex: 1;
+  margin-left: 12px;
+`;
+
+const ItemStudent = styled.Text`
+  padding-top: 3px;
+  padding-left: 3px;
+`;
+
+const ItemTitle = styled.Text.attrs(() => ({
+  numberOfLines: 1,
+}))`
+  width: 200px;
+  font-size: 20px;
+  font-weight: 600;
+`;
+
 const ItemRowContainer = styled.View`
   flex: 1;
-  padding-bottom: 10px;
+  padding-top: 10px;
   flex-direction: row;
   width: 100%;
+`;
+const ItemComment = styled.Text`
+  padding-left: 5px;
+  color: ${({ theme }) => theme.listTime};
+`;
+const ItemTime = styled.Text`
+  font-size: 12px;
+  color: ${({ theme }) => theme.listTime};
+  position: absolute;
+  right: 0px;
+  padding-top: 20px;
 `;
 
 const ImageContanier = styled.Image`
@@ -51,224 +61,125 @@ const ImageContanier = styled.Image`
   border-radius: 10px;
 `;
 
-const TitleText = styled.Text`
-  padding-right: 40px;
-  font-size: 24px;
-  font-weight: 600;
-  line-height: 30px;
-  color: ${({ theme }) => theme.marketFont};
-`;
-
-const ContentText = styled.Text`
-  font-size: 16px;
-  padding: 3px;
-  color: ${({ theme }) => theme.marketFont};
-`;
-
 const StatusText = styled.Text`
   padding-top: 5px;
   margin-left: auto;
   font-size: 16px;
 `;
 
-const DateText = styled.Text`
-  padding-top: 5px;
-  margin-left: auto;
-  font-size: 13px;
-`;
+const Item = React.memo(
+  // 같은내용이 리렌더링되는것을 막아준다.
+  ({ item }) => {
+    const theme = useContext(ThemeContext);
 
-const Markets = ({ navigation }) => {
-  const boards = [
-    {
-      num: 1,
-      title: "her",
-      hit: 11,
-      price: "69,000",
-      commentCount: 10,
-      thumnail: images.her,
-      nickname: "작성자1",
-      description: "she love me",
-      status: 0,
-      inDate: "2021.04.23",
-    },
-    {
-      id: 2,
-      title: "극한직업",
-      price: "59,000",
-      commentCount: 15,
-      inDate: "2021.04.23",
-      description: "수원 왕갈비 통닭",
-      status: 1,
-      thumnail: images.hardwork,
-      nickname: "작성자2",
-    },
-    {
-      id: 3,
-      title: "avengers",
-      price: "99,000",
-      commentCount: 30,
-      inDate: "2021.04.23",
-      description: "all die",
-      status: 0,
-      thumnail: images.avengers,
-      nickname: "작성자3",
-    },
-    {
-      id: 4,
-      title: "avengers",
-      price: "99,000",
-      commentCount: 30,
-      inDate: "2021.04.23",
-      description: "all die",
-      status: 1,
-      thumnail: images.avengers,
-      nickname: "작성자3",
-    },
-    {
-      id: 6,
-      title: "avengers",
-      price: "99,000",
-      commentCount: 30,
-      inDate: "2021.04.23",
-      description: "all die",
-      status: 2,
-      thumnail: images.avengers,
-      nickname: "작성자3",
-    },
-  ];
-
-  const categoriesData = [
-    {
-      id: 1,
-      categoryName: "교재",
-      boards: boards,
-    },
-    {
-      id: 2,
-      categoryName: "it기기",
-      boards: boards,
-    },
-    {
-      id: 3,
-      categoryName: "무료나눔",
-      boards: boards,
-    },
-  ];
-
-  const [categories, setCategories] = useState(categoriesData);
-  const [selectedCategory, setSelectedCategory] = useState(1);
-
-  function renderCategoryHeader() {
-    const renderItem = ({ item }) => {
-      return (
-        <CategoryContainer onPress={() => setSelectedCategory(item.id)}>
-          {selectedCategory == item.id && (
-            <ClickedCategoryText>{item.categoryName}</ClickedCategoryText>
-          )}
-          {selectedCategory != item.id && (
-            <CategoryText>{item.categoryName}</CategoryText>
-          )}
-        </CategoryContainer>
-      );
-    };
-    return (
-      <View>
-        <FlatList
-          data={categories}
-          showsHorizontalScrollIndicator={false}
-          renderItem={renderItem}
-          keyExtractor={(item) => `${item.id}`}
-          horizontal
-        />
-      </View>
-    );
-  }
-
-  function renderCategoryData() {
-    let boards = [];
-
-    let selectedCategoryBoards = categories.filter(
-      (category) => category.id == selectedCategory
-    );
-
-    if (selectedCategoryBoards.length) {
-      boards = selectedCategoryBoards[0].boards;
-    }
     const changeStatus = ({ item }) => {
       if (item.status === 0) return "판매중";
       if (item.status === 1) return "예약중";
       if (item.status === 2) return "판매완료";
     };
 
-    const renderItem = ({ item }) => {
-      return (
-        <View style={{ marginVertical: 8 }}>
-          <ItemContainer
-            onPress={() =>
-              navigation.navigate("Detail", {
-                board: item,
-              })
-            }
-          >
-            <ImageContanier source={item.thumnail} resizeMode="cover" />
-            <View style={{ flex: 1, marginLeft: 12 }}>
-              <View>
-                <ItemRowContainer>
-                  <TitleText>{item.title}</TitleText>
-                  <StatusText>{changeStatus({ item })}</StatusText>
-                </ItemRowContainer>
-                <ItemRowContainer>
-                  <MaterialIcons name="person" size={25} />
-                  <ContentText>{item.nickname}</ContentText>
-                </ItemRowContainer>
-                <ItemRowContainer>
-                  <MaterialIcons name="payment" size={25} />
-                  <ContentText>{item.price}원</ContentText>
-                </ItemRowContainer>
-
-                <ItemRowContainer>
-                  <MaterialIcons name="comment" size={25} />
-                  <ContentText>{item.commentCount}</ContentText>
-                  <DateText>{item.inDate}</DateText>
-                </ItemRowContainer>
-              </View>
-            </View>
-          </ItemContainer>
-        </View>
-      );
-    };
     return (
-      <View style={{ flex: 1, marginTop: 12 }}>
-        <FlatList
-          data={boards}
-          renderItem={renderItem}
-          keyExtractor={(item) => `${item.id}`} //각 키에 고유 키
-          showsVerticalScrollIndicator={false} //스크롤 표시 X
-        />
+      <View style={{ marginVertical: 8 }}>
+        <ItemContainer
+          onPress={() =>
+            navigation.navigate("ViewDetail", {
+              board: item,
+            })
+          }
+        >
+          <ImageContanier source={{ uri: item.thumbnail }} resizeMode="cover" />
+          <ItemTextContainer>
+            <View>
+              <ItemRowContainer>
+                <ItemTitle>{item.title}</ItemTitle>
+                <StatusText>{changeStatus({ item })}</StatusText>
+              </ItemRowContainer>
+              <ItemRowContainer>
+                <MaterialIcons name="person" size={24} color={theme.listIcon} />
+                <ItemStudent>{item.nickname}</ItemStudent>
+              </ItemRowContainer>
+              <ItemRowContainer>
+                <MaterialIcons name="payment" size={25} />
+                <ItemStudent>{item.price}원</ItemStudent>
+              </ItemRowContainer>
+              <ItemRowContainer>
+                <FontAwesome5 name="comment-dots" size={22} color="black" />
+                <ItemComment>{item.commentCount}</ItemComment>
+                <ItemTime>{item.inDate}</ItemTime>
+              </ItemRowContainer>
+            </View>
+          </ItemTextContainer>
+        </ItemContainer>
       </View>
     );
   }
+);
+
+function FreeBoard({ navigation }) {
+  const [isReady, setIsReady] = useState(false);
+  const [boards, setBoards] = useState([]);
+
+  const { spinner } = useContext(ProgressContext);
+
+  const _loadBoards = async () => {
+    try {
+      spinner.start();
+
+      const config = {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      };
+
+      const response = await fetch(
+        "http://13.125.55.135:9800/api/boards/book",
+        config
+      );
+      const json = await response.json();
+      json.success ? setBoards(json.boards) : Alert.alert(json.msg);
+    } catch (e) {
+      Alert.alert("실패", e.message);
+    } finally {
+      spinner.stop();
+    }
+  };
+  useEffect(() => {
+    _loadBoards();
+  }, []);
+
+  const _handleItemPress = (params) => {
+    navigation.navigate("ViewDetail", params);
+  };
   const _handleWritePress = (params) => {
     navigation.navigate("PostWrite", params);
   };
 
-  return (
+  return isReady ? (
     <Container>
-      <ScrollView style={{ marginTop: 12 }}>
-        <View style={{ marginTop: 12 }}>
-          <View>{renderCategoryHeader()}</View>
-          <View>{renderCategoryData()}</View>
-        </View>
-      </ScrollView>
+      <FlatList
+        keyExtractor={(item) => `${item.id}`}
+        data={boards}
+        renderItem={({ item }) => (
+          <Item item={item} onPress={_handleItemPress} />
+        )}
+        windowSize={3} // 렌더링 되는양을 조절
+      />
       <FAB
         buttonColor="#e84118"
         iconTextColor="#ffffff"
-        // onClickAction={_handleWritePress}
+        onClickAction={_handleWritePress}
         visible={true}
         iconTextComponent={<MaterialIcons name="edit" />}
       />
     </Container>
+  ) : (
+    <AppLoding
+      startAsync={_loadBoards}
+      onFinish={() => setIsReady(true)}
+      onError={console.error}
+    />
   );
-};
+}
 
-export default Markets;
+export default FreeBoard;
