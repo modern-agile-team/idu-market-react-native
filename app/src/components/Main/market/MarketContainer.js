19c82aab0c71f1de1d2672c74df1e61273026512
박우림 <woorimprog@gmail.com>
2021-05-04 17:09:33 +0900

    Add(woorim/main): 카테고리별로 데이터 불러오도록 기능 추가

---
+++ app/src/components/Main/market/MarketContainer.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components/native";
+
+import Topic from "../Topic";
+import ItemList from "./ItemList";
+
+const Container = styled.View`
+  flex: 1;
+`;
+
+const MarketContainer = ({
+  category,
+  topicTitle,
+  topicFontSize,
+  hitSlop,
+  topicOnPress,
+  itemOnPress,
+}) => {
+  return (
+    <Container>
+      <Topic
+        title={topicTitle}
+        fontSize={topicFontSize}
+        hitSlop={hitSlop}
+        onPress={topicOnPress}
+      />
+      <ItemList category={category} hitSlop={hitSlop} onPress={itemOnPress} />
+    </Container>
+  );
+};
+
+export default MarketContainer;
