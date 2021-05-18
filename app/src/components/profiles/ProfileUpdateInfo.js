5c80be246570d9f072e6aa91469d554177a6a337
jsj <tw450497@gmail.com>
2021-05-18 11:03:25 +0900

    Merge(sangjun/profile): develop merge 후 분리된 major파일 사용

--- app/src/components/profiles/ProfileUpdateInfo.js
+++ app/src/components/profiles/ProfileUpdateInfo.js
@@ -12,6 +12,7 @@ import {
 import { Input } from "../../components";
 import { getItemFromAsync } from "../../utils/AsyncStorage";
 import { ProgressContext, ReadyContext } from "../../contexts";
+import majors from "../../utils/majors";
 
 const Container = styled.View`
   flex: 1;
@@ -49,115 +50,7 @@ const ErrorText = styled.Text`
   color: ${({ theme }) => theme.errorText};
 `;
 
-const majors = [
-  {
-    item: "비서학과",
-    value: "1",
-  },
-  {
-    item: "관광서비스경영학과",
-    value: "2",
-  },
-  {
-    item: "휴먼사회복지학과",
-    value: "3",
-  },
-  {
-    item: "비지니스영어과",
-    value: "4",
-  },
-  {
-    item: "비즈니스중국어과",
-    value: "5",
-  },
-  {
-    item: "비지니스일본어과",
-    value: "6",
-  },
-  {
-    item: "세무회계학과",
-    value: "7",
-  },
-  {
-    item: "글로벌항공서비스학과",
-    value: "8",
-  },
-  {
-    item: "건축학과",
-    value: "9",
-  },
-  {
-    item: "토목공학과",
-    value: "10",
-  },
-  {
-    item: "실내건축과",
-    value: "11",
-  },
-  {
-    item: "디지털산업디자인학과",
-    value: "12",
-  },
-  {
-    item: "시각디자인과",
-    value: "13",
-  },
-  {
-    item: "주얼리디자인학과",
-    value: "14",
-  },
-  {
-    item: "멀티미디어디자인학과",
-    value: "15",
-  },
-  {
-    item: "정보통신공학과",
-    value: "16",
-  },
-  {
-    item: "리빙세라믹디자인학과",
-    value: "17",
-  },
-  {
-    item: "게임/vr디자인학과",
-    value: "18",
-  },
-  {
-    item: "방송영상미디어학과",
-    value: "19",
-  },
-  {
-    item: "방송뷰티학과",
-    value: "20",
-  },
-  {
-    item: "기계자동화학과",
-    value: "21",
-  },
-  {
-    item: "컴퓨터전자공학과",
-    value: "22",
-  },
-  {
-    item: "산업경영공학과",
-    value: "23",
-  },
-  {
-    item: "컴퓨터소프트웨어학과",
-    value: "24",
-  },
-  {
-    item: "메카트로닉스공학과",
-    value: "25",
-  },
-  {
-    item: "융합기계공학과",
-    value: "26",
-  },
-];
-
 const ProfileUpdateInfo = ({ isNickname, isEmail, major, navigation }) => {
-  const [disabled, setDisabled] = useState(true);
   const [profile, setProfile] = useState({});
   const [nickname, setIsNickname] = useState(isNickname);
   const [email, setIsEmail] = useState(isEmail);
@@ -237,11 +130,9 @@ const ProfileUpdateInfo = ({ isNickname, isEmail, major, navigation }) => {
       let _errorMessage = "";
       if (!checkNickname(nickname)) {
         _errorMessage = "별명은 최대 8글자입니다 (모음,자음 불가)";
-      }
-      // else if (!validateEmail(email)) {
-      // _errorMessage = "이메일 형식을 지켜주세요";
-      // }
-      else {
+      } else if (!validateEmail(email)) {
+        _errorMessage = "이메일 형식을 지켜주세요";
+      } else {
         _errorMessage = "";
       }
       setErrorMessage(_errorMessage);
